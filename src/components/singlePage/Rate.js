820974c0f9073e900d5d65fc1f9af1f648eb470b
Hamidreza <htondkar@gmail.com>
2017-04-06 20:26:45 +0430

    progress on rate comp.

--- src/components/singlePage/Rate.js
+++ src/components/singlePage/Rate.js
@@ -10,27 +10,36 @@ export default class Rate extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      rate: 0
+      rate: 0,
+      userRated: false
     }
   }
+  handleRate = (event) => {
+    this.setState({rate: event.target.id, userRate: true});
+
+  }
   render() {
+    let ur = {
+      rated: this.state.rate !== 0,
+      rate: this.state.rate
+    }
     return (
       <div className="single-page-rate">
         <span>
           <ToggleStar style={styles.svgStar} />
           <span>rate {this.props.name}</span>
         </span>
-        <div className="rate-box-container">
-          <div className="rate-box">1</div>
-          <div className="rate-box">2</div>
-          <div className="rate-box">3</div>
-          <div className="rate-box">4</div>
-          <div className="rate-box">5</div>
-          <div className="rate-box">6</div>
-          <div className="rate-box">7</div>
-          <div className="rate-box">8</div>
-          <div className="rate-box">9</div>
-          <div className="rate-box">10</div>
+        <div className="rate-box-container" onClick={this.handleRate}>
+          <div id='1' className="rate-box">1</div>
+          <div id='2' className="rate-box">2</div>
+          <div id='3' className="rate-box">3</div>
+          <div id='4' className="rate-box">4</div>
+          <div id='5' className="rate-box">5</div>
+          <div id='6' className="rate-box">6</div>
+          <div id='7' className="rate-box">7</div>
+          <div id='8' className="rate-box">8</div>
+          <div id='9' className="rate-box">9</div>
+          <div id='10' className="rate-box">10</div>
         </div>
       </div>
     )
