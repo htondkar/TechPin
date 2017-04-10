7ea238343540bbf7dbc45ef7d0e477e3b718c69d
Hamidreza <htondkar@gmail.com>
2017-04-10 11:28:11 +0430

    fixes meta tags

--- src/components/staticPages/AboutPage.js
+++ src/components/staticPages/AboutPage.js
@@ -26,7 +26,7 @@ const AboutPage = (props) => {
             <p>{aboutText.whatIsRating}</p>
             <h3>What is NPS Score?</h3>
             <p>{aboutText.NPS}</p>
-
+            
             <p><a href="http://www.medallia.com/net-promoter-score/">read more about this rating</a></p>
             <p>TechPin initial codebase was contributed by <a href="http://www.idearun.co/">idearun Startup Studio</a> in December 2016</p>
           </div>
